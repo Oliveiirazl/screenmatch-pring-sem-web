2c355ac745c7766aefd01ecf1adfcfbefffb3dc5
Lucas Oliveira <lo8304179@gmail.com>
2025-05-04 16:17:14 -0300

    Implementado Temporadas

--- src/main/java/br/com/api/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/api/screenmatch/ScreenmatchApplication.java
@@ -2,11 +2,15 @@ package br.com.api.screenmatch;
 
 import br.com.api.screenmatch.model.DadosEpisodiosModel;
 import br.com.api.screenmatch.model.DadosSerieModel;
+import br.com.api.screenmatch.model.DadosTemporadaModel;
 import br.com.api.screenmatch.service.ConsumoApiService;
 import br.com.api.screenmatch.service.ConverteDadosService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -27,6 +31,14 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=2274833f");
 		DadosEpisodiosModel dadosEpisodio = conversor.obterDados(json, DadosEpisodiosModel.class);
 		System.out.println(dadosEpisodio);
+
+		List<DadosTemporadaModel> temporadas = new ArrayList<>();
+
+		for (int i = 1; i <= dados.totalTemporadas() ; i++) {
+			json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season=" + i + "&&apikey=2274833f");
+			DadosTemporadaModel dadosTemporada = conversor.obterDados(json, DadosTemporadaModel.class);
+			temporadas.add(dadosTemporada);
+		} temporadas.forEach(System.out::println);
 
 
 
---
+++ src/main/java/br/com/api/screenmatch/model/DadosTemporadaModel.java
@@ -0,0 +1,9 @@
+package br.com.api.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosTemporadaModel(@JsonAlias("Season") Integer numero, @JsonAlias("Episodes") List<DadosEpisodiosModel> episodios) {
+}
